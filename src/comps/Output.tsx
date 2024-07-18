2f00873c45b6de0e72e14fb005f1a934be50827e
heartsgom <litearma@gmail.com>
2024-07-19 03:14:47 +0800

    modified content

--- src/comps/Output.tsx
+++ src/comps/Output.tsx
@@ -1,15 +1,20 @@
+import './Comps.css'
+
 export default function Output(){
     return (
-        <div className = "Output">
-            <h1>My Output</h1>
-            "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born
-            and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, 
-            the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but 
-            because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. 
-            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally 
-            circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever 
-            undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man 
-            who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
-        </div>
+        <>
+            <div className = "Output">
+                <h1>My Output</h1>
+                <p>During my internship at Fasttrack, I was assigned to two projects: The CODEX Case Study and a client project for Solutions Exchange,
+                    one of Cloudtech's clients. The Case Study focused on script debugging and utilizing Advanced PDF/HTML Templates, Netsuite's 
+                    customizable forms powered by Apache Freemarker, a Java-based template engine. Netsuite's scripting language, SuiteScript, 
+                    is based on JavaScript and is used for batch processes, custom validations and calculations, executing searches, and performing CRUD operations.
+                </p>
+                <p>
+                    In the Solutions Exchange project I was assigned to the CAS Configuration where I had to adjust the templates per the client's requests.
+                    Some of the templates I adjusted were the templates for the Sales Invoice, Collection Receipt, and Credit Memo.
+                </p>
+            </div>
+        </>
     )
 }
--- src/comps/Overview.tsx
+++ src/comps/Overview.tsx
@@ -1,20 +1,14 @@
+import './Comps.css'
+
 export default function Overview(){
     return (
-        <div className = "Overview">
-            <h1>An Overview of my Practicum</h1>
-            I was accepted as an intern at Fasttrack Solutions Inc., located in the Valero Tower in Makati City. Fasttrack is a software and consulting company specializing in 
-            cloud technology implementation, ERP/CRM deployment, and vertical solutions. While they are best known for their SAP implementations, they have also expanded to other 
-            ERP systems, including Acumatica and Smartbooks. Additionally, they offer business analytics services, such as data warehousing, data transformation, and data 
-            visualization. With over 18 years of operation, Fasttrack has offices in Cebu, Davao, and internationally in Singapore. They serve various industries and count 
-            the Ayala Foundation, Unilab, Sanicare, and Healthy Options among their key business partners. <br /><br /> The company has developed a training program called the Consultant 
-            Online Development and Excellence Program (CODEX), designed to nurture high-potential talents with in-demand IT skills. I was assigned to the Oracle NetSuite ERP 
-            Program Track, managed by Cloudtech ERP, a Fasttrack subsidiary focused on Oracle, NetSuite, and Salesforce. <br /><br /> The practicum consists of three phases. The first phase, 
-            General IT and Soft Skills, covers fundamental concepts such as programming basics, communication skills, SQL, and proficiency with Excel and Microsoft Visio. Each 
-            phase concludes with an assessment that must be passed to proceed. The second phase, Product-Specific Training, involves mastering the system we will be using. The 
-            final phase, Work Immersion and Case Study, requires completing a case study prepared by our supervisor and potentially being deployed to assist on a real project.
-            168 hours were rendered during the CODEX Training, and 156 hours during the Work Immersion. The total amount of hours rendered was 324 hours, completing the minimum 
-            requirement for work hours rendered. The internship started the 22nd of May, 2024, and ended the 21st of June, 2024.
-
-        </div>
+        <>
+            <div className = "Overview">
+                <h1>An Overview of my Practicum</h1>
+                <p>I worked as an intern at Fasttrack Solutions Inc. a software and consulting company specializing in cloud technology and ERP software located in Makati.</p>
+                <p>I was assigned as a Technical Consultant intern under the Oracle Netsuite ERP Program managed by Cloudtech ERP, a subsidiary company primarily focused on Oracle Netsuite and Salesforce</p>
+                <p>My internship started the 22nd of May, 2024 and ended the 21st of June, 2024, rendering a total of 324 work hours.</p>
+            </div>
+        </>
     )
 }
