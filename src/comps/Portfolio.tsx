c320dd59833024b69f964a45ce5bb5bc4e96e9ab
heartsgom <litearma@gmail.com>
2024-07-20 05:42:14 +0800

    portfolio

--- src/comps/Portfolio.tsx
+++ src/comps/Portfolio.tsx
@@ -1,11 +1,15 @@
 import './Comps.css'
+import Projects from './Projects'
 
 export default function Portfolio(){
     return (
         <>
-            <div className = "Overview">
+            <div className = "AboutMe">
                 <h1>Hi. I'm Mark Nacionales</h1>
-                <p></p>
+                <p>I am a passionate computer science student at Mapúa Malayan Colleges Laguna, where I am diving deep into the world of technology and innovation. My academic journey is driven by a keen interest in solving complex problems through coding and software development. Alongside my studies, I enjoy exploring emerging technologies and contributing to projects that merge technical expertise with practical solutions.</p>
+            </div>
+            <div className='ProjectsSection'>
+                <Projects />
             </div>
         </>
     )
---
+++ src/comps/philculcard.tsx
@@ -0,0 +1,12 @@
+export default function FilculCard(){
+    return (
+        <>
+            <p>Designed, developed and implemented a
+                Membership Management System web application for PHILCUL
+                association that allowed member registration, and personal information management
+                as well as administrator encoding, inquiry, and payment modules.
+                Developed using bootstrap for the front-end, and C# and Azure SQL for the back-end.
+            </p>
+        </>
+    )
+}
